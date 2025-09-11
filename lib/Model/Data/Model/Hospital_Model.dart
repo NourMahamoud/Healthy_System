52130153d607b8df9a15b28157988623c26b82ee
Ziyad-000 <Ziyad.sayed1515@compit.aun.edu.eg>
2025-09-11 17:31:09 +0300

    Refactor Hospital_Model and add image assets

--- lib/Model/Data/Model/Hospital_Model.dart
+++ lib/Model/Data/Model/Hospital_Model.dart
@@ -1,12 +1,38 @@
-class Hospital_Model {
-  final String  name ;
-  final String email ;
-  final  String  phoneNumber ;
-  final String _password ;
-  final String address ;
-  final String  id ;
+class HospitalModel {
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final String _password; // Password is should be Private
+  final String address;
+  final String id;
 
-  Hospital_Model(this.name, this.email, this.phoneNumber, this._password,
-      this.address, this.id);
+  HospitalModel({
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required String password,
+    required this.address,
+    required this.id,
+  }) : _password = password; // Save the password
 
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'address': address,
+      'id': id,
+    };
+  }
+
+  factory HospitalModel.fromMap(Map<String, dynamic> map) {
+    return HospitalModel(
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      phoneNumber: map['phoneNumber'] ?? '',
+      password: '',
+      address: map['address'] ?? '',
+      id: map['id'] ?? '',
+    );
+  }
 }
